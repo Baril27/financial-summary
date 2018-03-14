732dc9e813d244dbade8ad38b636d40a6831cbc7
Eric Baril <baril27@gmail.com>
2018-03-14 12:08:54 -0700

    Add logic to FinancialSummary class and build out specs for all transaction types.

--- app/models/financial_summary.rb
+++ app/models/financial_summary.rb
@@ -1,3 +1,46 @@
 class FinancialSummary
 
+  class << self
+
+    attr_accessor :transactions
+
+    def one_day params
+      transactions = Transaction.where(user: user(params), amount_currency: currency(params))
+                                .where('created_at >= ?', 1.day.ago)
+      self.transactions = transactions
+      self
+    end
+
+    def seven_days params
+      transactions = Transaction.where(user: user(params), amount_currency: currency(params))
+                                .where('created_at >= ?', 7.days.ago)
+      self.transactions = transactions
+      self
+    end
+
+    def lifetime params
+      transactions = Transaction.where(user: user(params), amount_currency: currency(params))
+      self.transactions = transactions
+      self
+    end
+
+    def count category
+      transactions.where(category: category).count
+    end
+
+    def amount category
+      transactions.where(category: category).sum(&:amount)
+    end
+
+    private
+
+    def user params
+      params[:user]
+    end
+
+    def currency params
+      params[:currency].to_s.upcase
+    end
+  end
+
 end
--- spec/models/financial_summary_spec.rb
+++ spec/models/financial_summary_spec.rb
@@ -1,60 +1,74 @@
 require 'rails_helper'
 
 describe FinancialSummary do
-  it 'summarizes over one day' do
-    pending('Not implemented yet')
 
-    user = create(:user)
+  shared_examples "summarizes over one day" do |category|
+    let(:category) { category }
 
-    Timecop.freeze(Time.now) do
-      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
-      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :usd))
+    it "for #{category}" do
+      user = create(:user)
+
+      Timecop.freeze(Time.now) do
+        create(:transaction, user: user, category: category, amount: Money.from_amount(2.12, :usd))
+        create(:transaction, user: user, category: category, amount: Money.from_amount(10, :usd))
+      end
+
+      Timecop.freeze(2.days.ago) do
+        create(:transaction, user: user, category: category)
+      end
+
+      subject = FinancialSummary.one_day(user: user, currency: :usd)
+      expect(subject.count(category)).to eq(2)
+      expect(subject.amount(category)).to eq(Money.from_amount(12.12, :usd))
     end
-
-    Timecop.freeze(2.days.ago) do
-      create(:transaction, user: user, category: :deposit)
-    end
-
-    subject = FinancialSummary.one_day(user: user, currency: :usd)
-    expect(subject.count(:deposit)).to eq(2)
-    expect(subject.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
   end
 
-  it 'summarizes over seven days' do
-    pending('Not implemented yet')
+  shared_examples "summarizes over seven days" do |category|
+    let(:category) { category }
 
-    user = create(:user)
+    it "for #{category}" do
+      user = create(:user)
 
-    Timecop.freeze(5.days.ago) do
-      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
-      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :usd))
+      Timecop.freeze(5.days.ago) do
+        create(:transaction, user: user, category: category, amount: Money.from_amount(2.12, :usd))
+        create(:transaction, user: user, category: category, amount: Money.from_amount(10, :usd))
+      end
+
+      Timecop.freeze(8.days.ago) do
+        create(:transaction, user: user, category: category)
+      end
+
+      subject = FinancialSummary.seven_days(user: user, currency: :usd)
+      expect(subject.count(category)).to eq(2)
+      expect(subject.amount(category)).to eq(Money.from_amount(12.12, :usd))
     end
-
-    Timecop.freeze(8.days.ago) do
-      create(:transaction, user: user, category: :deposit)
-    end
-
-    subject = FinancialSummary.seven_days(user: user, currency: :usd)
-    expect(subject.count(:deposit)).to eq(2)
-    expect(subject.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
   end
 
-  it 'summarizes over lifetime' do
-    pending('Not implemented yet')
+  shared_examples "summarizes over lifetime" do |category|
+    let(:category) { category }
 
-    user = create(:user)
+    it "for #{category}" do
+      user = create(:user)
 
-    Timecop.freeze(30.days.ago) do
-      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
-      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :usd))
+      Timecop.freeze(30.days.ago) do
+        create(:transaction, user: user, category: category, amount: Money.from_amount(2.12, :usd))
+        create(:transaction, user: user, category: category, amount: Money.from_amount(10, :usd))
+      end
+
+      Timecop.freeze(8.days.ago) do
+        create(:transaction, user: user, category: category)
+      end
+
+      subject = FinancialSummary.lifetime(user: user, currency: :usd)
+      expect(subject.count(category)).to eq(3)
+      expect(subject.amount(category)).to eq(Money.from_amount(13.12, :usd))
     end
-
-    Timecop.freeze(8.days.ago) do
-      create(:transaction, user: user, category: :deposit)
-    end
-
-    subject = FinancialSummary.lifetime(user: user, currency: :usd)
-    expect(subject.count(:deposit)).to eq(3)
-    expect(subject.amount(:deposit)).to eq(Money.from_amount(13.12, :usd))
   end
+
+  ["deposit", "refund", "withdraw"].each do |category|
+    include_examples "summarizes over one day", category
+    include_examples "summarizes over seven days", category
+    include_examples "summarizes over lifetime", category
+  end
+
 end
